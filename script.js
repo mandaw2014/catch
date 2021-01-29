ffe0c39d0e7484c22fe87bc2a1f37d0700dba809
mandaw2014 <77012627+mandaw2014@users.noreply.github.com>
2021-01-29 10:27:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ body = document.getElementById("body");
 title = document.getElementById("title");
 
 player.addEventListener("mousedown", function(e) {
-    isDown = true;
+   isDown = true;
     offset = [
         player.offsetLeft - e.clientY,
         player.offsetTop - e.clientX
     ];
-}, true);
+}, true); 
 
 
 document.addEventListener("mousemove", function(event) {
@@ -44,6 +44,52 @@ var score = 0;
 var highscore = localStorage.getItem("highscore/catch", 0);
 
 function highScore () {
+    if (highscore != null) {
+        if (score > highscore) {
+            localStorage.setItem("highscore/catch", score);
+        }
+    } else {
+        localStorage.setItem("highscore/catch", score);
+    }
+}
+
+function moveBlock(){
+    block.style.position = 'absolute';
+    block.style.top = Math.floor(Math.random() * 90 + 5) + '%';
+    block.style.left = Math.floor(Math.random() * 90 + 5) + '%';
+}
+
+function gameLost() {
+    alert("Game Over" + "      Score: " + score + "     Highscore: " + highscore);
+    location.reload();
+}
+
+var is_colliding = function() {
+    var d1_height = player.offsetHeight;
+    var d1_width = player.offsetWidth;
+    var d1_distance_from_top = player.offsetTop + d1_height;
+    var d1_distance_from_left = player.offsetLeft + d1_width;
+  
+    var d2_height = block.offsetHeight;
+    var d2_width = block.offsetWidth;
+    var d2_distance_from_top = block.offsetTop + d2_height;
+    var d2_distance_from_left = block.offsetLeft + d2_width;
+  
+    var not_colliding =
+      d1_distance_from_top < block.offsetTop ||
+      player.offsetTop > d2_distance_from_top ||
+      d1_distance_from_left < block.offsetLeft ||
+      player.offsetLeft > d2_distance_from_left;
+
+    if (!not_colliding) {
+        console.log("collide");
+        moveBlock();
+        highScore();
+        score++;
+    }
+
+    return !not_colliding;
+};function highScore () {
     if (highscore != null) {
         if (score > highscore) {
             localStorage.setItem("highscore/catch", score);
